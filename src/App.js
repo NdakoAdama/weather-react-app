160b5d00e17709685423d638e9dbf64cdac243cd
NdakoAdama <adamamuhammad01@gmail.com>
2024-07-12 03:27:23 -1200

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather
+      <h1>Weather</h1>
+      <footer>
+        This project was coded by Ndako Adama and is open sourced on{" "}
+        <a
+          href="https://github.com/NdakoAdama/weather-react-app"
+          target="_blank"
+        >
+          Github
+        </a>{" "}
+        and hosted on
+        <a href="https://weatherapprct.netlify.app" target="_blank">
+          {" "}
+          netlify.
+        </a>
+      </footer>
     </div>
   );
 }
-
-
